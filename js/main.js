6abb4825d902934c28281272ff01726240c10e37
fehoffcial <feliphe@tuta.io>
2022-12-19 01:29:17 -0300

    Projects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-alert("112")
+let initName = document.querySelector("#exampleInputName");
+let initGeneroM = document.querySelector("#exampleInputGenderM");
+let initGeneroF = document.querySelector("#exampleInputGenderF");
+let initControy = document.querySelector("#exampleInputCountry");
+let initEmail = document.querySelector("#exampleInputEmail1");
+let initPassword = document.querySelector("#exampleInputPassword1");
